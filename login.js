545f967ffe4300a104c5fd41c43f117ef7cb9af9
Pashakamal58168 <pashakamal2004@gmail.com>
2024-06-06 07:51:24 -0700

    done

---
+++ login.js
@@ -0,0 +1,9 @@
+
+let userInput = prompt("Enter username:");
+let passInput = +prompt("Enter password:");
+
+if (userInput==="mustafa" || passInput ===1234) {
+    alert("Login successful!");
+} else {
+    alert("Incorrect username or password. Please try again.");
+}
